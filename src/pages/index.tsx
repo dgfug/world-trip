42fde2e168c4e8d7ae7839c01a9d18d932fb91fa
Douglas Scaini <douglas.scaini@unoesc.edu.br>
2021-10-05 13:19:49 -0300

    feat: Create Banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Flex } from "@chakra-ui/react";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <Flex w="100vw" h="100vh">
+    <Flex w="100vw" h="100vh" direction="column">
       <Header />
+      <Banner />
     </Flex>
   );
 }
