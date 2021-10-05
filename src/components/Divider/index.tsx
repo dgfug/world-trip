408b69903941138f0827aa788518131a9cbb3e05
Douglas Scaini <douglas.scaini@unoesc.edu.br>
2021-10-05 17:25:20 -0300

    feat: Divider Component and Center Item

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,5 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Divider() {
+  return <Box w="90px" h="2px" bg="gray.450" />;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Center, Text } from "@chakra-ui/react";
 import Banner from "../components/Banner";
+import Divider from "../components/Divider";
 import Header from "../components/Header";
 import TravelTypes from "../components/TravelTypes";
 
@@ -9,6 +10,16 @@ export default function Home() {
       <Header />
       <Banner />
       <TravelTypes />
+
+      <Center flexDirection="column">
+        <Divider />
+        <Text mt="14" fontSize="4xl" fontWeight="500" color="gray.450">
+          Vamos Nessa?
+        </Text>
+        <Text t="14" fontSize="4xl" fontWeight="500" color="gray.450">
+          Então escolha seu continente
+        </Text>
+      </Center>
     </Flex>
   );
 }
