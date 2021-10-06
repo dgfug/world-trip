f4f637e143fb31f47063435d1d9571cb1b006225
Douglas Scaini <douglas.scaini@unoesc.edu.br>
2021-10-06 15:07:33 -0300

    chore: Banner Responsive

--- src/components/Banner/Content.tsx
+++ src/components/Banner/Content.tsx
@@ -1,23 +1,29 @@
 import { Flex, Text, Image, Box, Stack } from "@chakra-ui/react";
 
-export default function Content() {
+interface ContentProps {
+  isWideVersion?: boolean;
+}
+
+export default function Content({ isWideVersion }: ContentProps) {
   return (
     <Flex w="100%" maxWidth={1120} mx="auto" align="center" justify="space-between">
-      <Stack spacing="5">
-        <Text fontSize="4xl" fontWeight="500" color="gray.150">
+      <Stack spacing={["2", "5"]}>
+        <Text fontSize={["xl", "4xl"]} fontWeight="500" color="gray.150" pl={["4", "0"]}>
           5 Continentes,
           <br />
           Infinitas possibilidades.
         </Text>
 
-        <Text fontSize="xl" color="gray.250" maxW={550}>
+        <Text fontSize={["sm", "xl"]} color="gray.250" maxW={550} pl={["4", "0"]}>
           Chegou a hora de tirar do papel a viagem que você sempre sonhou.
         </Text>
       </Stack>
 
-      <Box>
-        <Image src="/airplane.svg" alt="Avião nas nuvens" transform="translateY(72px)" />
-      </Box>
+      {isWideVersion && (
+        <Box>
+          <Image src="/airplane.svg" alt="Avião nas nuvens" transform="translateY(72px)" />
+        </Box>
+      )}
     </Flex>
   );
 }
