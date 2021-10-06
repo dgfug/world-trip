45ada866a30bc4b3022db934200043fade2e8f3c
Douglas Scaini <douglas.scaini@unoesc.edu.br>
2021-10-06 17:45:42 -0300

    chore: Responsive TravelTypes

--- src/components/TravelTypes/index.tsx
+++ src/components/TravelTypes/index.tsx
@@ -1,14 +1,28 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import Content from "./Content";
 
 export default function TravelTypes() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
-    <Flex w="100%" maxWidth={1120} mx="auto" align="center" justify="space-between" mt="20" mb="20">
-      <Content iconName="cocktail" typeTravel="vida noturna" />
-      <Content iconName="surf" typeTravel="praia" />
-      <Content iconName="building" typeTravel="moderno" />
-      <Content iconName="museum" typeTravel="clássico" />
-      <Content iconName="earth" typeTravel="e mais..." />
+    <Flex
+      w="100%"
+      maxWidth={1120}
+      mx="auto"
+      pl={["12", "0"]}
+      pr={["12", "0"]}
+      mt={["9", "20"]}
+      mb={["9", "20"]}
+      flexWrap="wrap"
+    >
+      <Content iconName="cocktail" typeTravel="vida noturna" isWideVersion={isWideVersion} />
+      <Content iconName="surf" typeTravel="praia" isWideVersion={isWideVersion} />
+      <Content iconName="building" typeTravel="moderno" isWideVersion={isWideVersion} />
+      <Content iconName="museum" typeTravel="clássico" isWideVersion={isWideVersion} />
+      <Content iconName="earth" typeTravel="e mais..." isWideVersion={isWideVersion} />
     </Flex>
   );
 }
